15684e4c3d4cf11e519e12840d683bc9a35841dd
Appl3Kitti3 <tdumamag2@my.bcit.ca>
2023-05-01 18:41:05 -0700

    Uploaded new files.

--- MADNESS/README.txt
+++
@@ -1,4 +0,0 @@
-Please Put All of them in a File to work, thank you!
-
-
-[Two]
---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+
+loadSkeleton();
+
+
+// beyond this point are functions
+function loadSkeleton() {
+    console.log($('#navbarGuestPlaceHolder').load('./skeletons/nav.html'));
+}
